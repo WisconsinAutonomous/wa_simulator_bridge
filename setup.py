3c09eb0a0b242f1c1206dd3c65573574ac39be5d
AaronYoung5 <aarony3131@gmail.com>
2021-12-04 01:48:04 -0600

    Bridge working with some default message types

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "bridge = wa_simulator_ros2_bridge.bridge:main",
         ],
     },
 )
--- wa_simulator_ros2_bridge/bridge.py
+++ wa_simulator_ros2_bridge/bridge.py
@@ -2,9 +2,13 @@
 import rclpy
 from rclpy.node import Node
 from rcl_interfaces.msg import ParameterType
+from sensor_msgs.msg import Imu, NavSatFix
 
 # Import WA Simulator
 import wa_simulator as wa
+
+# External imports
+from typing import Dict, Callable, Any, List, NoReturn
 
 class WASimulatorROS2Bridge(Node):
 
@@ -16,6 +20,10 @@ class WASimulatorROS2Bridge(Node):
         self.publisher_handles = {}
         self.subscriber_handles = {}    
 
+        # Handles for callbacks
+        # Used whenever a message is received
+        self.message_callbacks = {}
+
         # -----------------------
         # Simulation bridge setup
         # -----------------------
@@ -24,12 +32,12 @@ class WASimulatorROS2Bridge(Node):
 
         # Create the bridge
         # The bridge is responsible for communicating the simulation
-        self.bridge = wa.WABridge(system, hostname="0.0.0.0", port=5555, server=False)
+        self.bridge = wa.WABridge(self.system, hostname="0.0.0.0", port=5555, server=False)
         self.bridge.add_receiver(message_parser=self.message_callback)
 
         # Create a simulation wrapper
         # Will be responsible for actually running the simulation
-        self.sim_manager = wa.WASimulationManager(system, bridge)
+        self.sim_manager = wa.WASimulationManager(self.system, self.bridge)
 
         # Periodic publishing
         timer_period = 0.01  # seconds
@@ -49,25 +57,122 @@ class WASimulatorROS2Bridge(Node):
             rclpy.shutdown()
 
         # Update the simulation to send and receive data
-        sim_manager.synchronize(self.system.time)
-        sim_manager.advance(self.system.step_size)
+        self.sim_manager.synchronize(self.system.time)
+        self.sim_manager.advance(self.system.step_size)
 
-    def message_callback(self, message: dict):
+    def message_callback(self, name: str, message: dict):
         """
         Receives all messages from the simulation
 
         When a new message is received, i.e. one with a unique name, a subscriber will be created
         """
-        print(message)
+        msg_type = message["type"]
+        data = message["data"]
+        if name not in self.message_callbacks:
+            if msg_type in self._publisher_creators and msg_type in self._message_callbacks:
+                self._publisher_creators[msg_type](self)
+                self.message_callbacks[name] = self._message_callbacks[msg_type]
+            else:
+                raise RuntimeError(f"Could not infer publisher creator for message type {msg_type}.")
 
+        handle = self.message_callbacks[name]
+        handle(self, data)
+        
+    # -----------------------------
+    # Inferrable publisher creators
+    # -----------------------------
+    _publisher_creators: Dict[str, Callable[['WASimulatorROS2Bridge'], NoReturn]] = {}
+            
+    def _create_publisher_WASystem(self):
+        global Time, Int32
+        from builtin_interfaces.msg import Time
+        from std_msgs.msg import Int32
+
+        self.publisher_handles["time"] = self.create_publisher(Time, "time", 1)
+        self.publisher_handles["step_number"] = self.create_publisher(Int32, "step_number", 1)
+    _publisher_creators["WASystem"] = _create_publisher_WASystem
+
+    def _create_publisher_WAVehicle(self):
+        global Vector3, Quaternion
+        from geometry_msgs.msg import Vector3, Quaternion
+
+        self.publisher_handles["position"] = self.create_publisher(Vector3, "position", 1)
+        self.publisher_handles["rotation"] = self.create_publisher(Quaternion, "rotation", 1)
+        self.publisher_handles["linear_velocity"] = self.create_publisher(Vector3, "linear_velocity", 1)
+        self.publisher_handles["angular_velocity"] = self.create_publisher(Quaternion, "angular_velocity", 1)
+        self.publisher_handles["linear_acceleration"] = self.create_publisher(Vector3, "linear_acceleration", 1)
+        self.publisher_handles["angular_acceleration"] = self.create_publisher(Quaternion, "angular_acceleration", 1)
+    _publisher_creators["WAVehicle"] = _create_publisher_WAVehicle
+
+    def _create_publisher_WAGPSSensor(self):
+        global NavSatFix
+        from sensor_msgs.msg import NavSatFix
+
+        self.publisher_handles["gps"] = self.create_publisher(NavSatFix, "gps", 1)
+    _publisher_creators["WAGPSSensor"] = _create_publisher_WAGPSSensor
+
+    def _create_publisher_WAIMUSensor(self):
+        global Imu
+        from sensor_msgs.msg import Imu
+
+        self.publisher_handles["imu"] = self.create_publisher(Imu, "imu", 1)
+    _publisher_creators["WAIMUSensor"] = _create_publisher_WAIMUSensor
+       
+    # ----------------------------
+    # Inferrable message callbacks
+    # ----------------------------
+    _message_callbacks: Dict[str, Callable[['WASimulatorROS2Bridge', dict], None]] = {}
+
+    def _message_callback_WASystem(self, message: dict):
+        self.publisher_handles["time"].publish(self._time_to_Time(message["time"]))
+        self.publisher_handles["step_number"].publish(Int32(data=message["step_number"]))
+    _message_callbacks['WASystem'] = _message_callback_WASystem
+
+    def _message_callback_WAVehicle(self, message: dict):
+        self.publisher_handles["position"].publish(self._WAVector_to_Vector3(message["position"]))
+        self.publisher_handles["rotation"].publish(self._WAQuaternion_to_Quaternion(message["rotation"]))
+        self.publisher_handles["linear_velocity"].publish(self._WAVector_to_Vector3(message["linear_velocity"]))
+        self.publisher_handles["angular_velocity"].publish(self._WAQuaternion_to_Quaternion(message["angular_velocity"]))
+        self.publisher_handles["linear_acceleration"].publish(self._WAVector_to_Vector3(message["linear_acceleration"]))
+        self.publisher_handles["angular_acceleration"].publish(self._WAQuaternion_to_Quaternion(message["angular_acceleration"]))
+    _message_callbacks['WAVehicle'] = _message_callback_WAVehicle
+
+    def _message_callback_WAGPSSensor(self, message: dict):
+        self.publisher_handles["gps"].publish(self._WAGPSSensor_to_NavSatFix(**message))
+    _message_callbacks['WAGPSSensor'] = _message_callback_WAGPSSensor
+
+    def _message_callback_WAIMUSensor(self, message: dict):
+        self.publisher_handles["imu"].publish(self._WAIMUSensor_to_Imu(**message))
+    _message_callbacks['WAIMUSensor'] = _message_callback_WAIMUSensor
+
+    # ---------------------------------------------
+    # WA Simulator data types to ROS msg converters
+    # ---------------------------------------------
+
+    def _time_to_Time(self, time: float):
+        sec = int(time)
+        return Time(sec=sec, nanosec=int((time-sec)*1e9)) 
+
+    def _WAVector_to_Vector3(self, vec: wa.WAVector):
+        return Vector3(x=vec.x, y=vec.y, z=vec.z)
+
+    def _WAQuaternion_to_Quaternion(self, q: wa.WAQuaternion):
+        return Quaternion(x=q.x, y=q.y, z=q.z, w=q.w)
+
+    def _WAGPSSensor_to_NavSatFix(self, latitude: float, longitude: float, altitude: float):
+        return NavSatFix(latitude=latitude, longitude=longitude, altitude=altitude)
+
+    def _WAIMUSensor_to_Imu(self, linear_acceleration: wa.WAVector, angular_velocity: wa.WAQuaternion, orientation: wa.WAQuaternion):
+        angular_velocity = wa.WAVector(angular_velocity.to_euler())
+        return Imu(orientation=self._WAQuaternion_to_Quaternion(orientation), angular_velocity=self._WAVector_to_Vector3(angular_velocity), linear_acceleration=self._WAVector_to_Vector3(linear_acceleration))
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    camera_publisher = CameraPublisher()
+    bridge = WASimulatorROS2Bridge()
 
-    rclpy.spin(camera_publisher)
+    rclpy.spin(bridge)
 
     rclpy.shutdown()
 
